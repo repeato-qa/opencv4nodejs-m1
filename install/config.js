ec6d43b285d1cc1a291d5a80a6a3f6ca0ca8d2b6
Simon Hofmann <mail@simon-hofmann.org>
2021-12-28 13:11:43 +0100

    Finished Windows build

--- install/config.js
+++ install/config.js
@@ -4,28 +4,32 @@ const { basename, join } = require("path");
 const fs = require("fs");
 
 const lib = {
-  win32: `opencv_world454.dll`,
+  win32: `opencv_world454.lib`,
   linux: `libopencv_world.so.4.5`,
   darwin: `libopencv_world.4.5.dylib`
 };
 
 const baseLibFolder = {
-  win32: `${__dirname}/../node_modules/@nut-tree/opencv-win32/x64/vc16/bin`,
-  linux: `${__dirname}/../node_modules/@nut-tree/opencv-linux/lib/`,
-  darwin: `${__dirname}/../node_modules/@nut-tree/opencv-darwin/lib/`
+  win32: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-win32", "x64", "vc16", "lib").replace(/\\/g, '\\\\'),
+  linux: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-linux", "lib"),
+  darwin: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-darwin", "lib")
 };
 
-const includeFolder = `${__dirname}/../node_modules/@nut-tree/opencv-${process.platform}/include/opencv4/`;
+const includeFolder = {
+  win32: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-win32", "include").replace(/\\/g, '\\\\'),
+  linux: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-linux", "include", "opencv4"),
+  darwin: join(__dirname, "..", "node_modules", "@nut-tree", "opencv-darwin", "include", "opencv4"),
+};
 
 const libDir = baseLibFolder[process.platform];
-const fullLibPath = join(libDir, lib[process.platform]);
+const fullLibPath = join(libDir, lib[process.platform]).replace(/\\/g, '\\\\');
 
 if (!fs.existsSync(libDir)) {
   throw new Error("library dir does not exist: " + libDir);
 }
 
 const defines = [`OPENCV4NODEJS_FOUND_LIBRARY_WORLD`];
-const includes = [includeFolder];
+const includes = [includeFolder[process.platform]];
 
 // linkLib produces linker flags for GNU ld and BSD ld
 // It generates linker flags based on the libPath, which make dealing with version numbers in lib names easier
